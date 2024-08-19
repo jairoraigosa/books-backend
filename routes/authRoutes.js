0f82c7c1ae17d87887dc288f80e6926d86a7c95d
jairoraigosa <46975709+jairoraigosa@users.noreply.github.com>
2024-08-18 21:42:33 -0500

    Ajuste de opciones que no se utilizarán

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,6 @@ const authenticateJWT = require('../middlewares/authenticateJWT');
 
 const router = express.Router();
 
-router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/verify', authenticateJWT, (req, res) => {
   res.json({ trans: true, message: 'Token validado con éxito', user: req.user });
